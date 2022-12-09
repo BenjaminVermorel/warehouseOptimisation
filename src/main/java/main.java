80458bb11708746c734661020b31415a48a488d3
benjamin <benvermorel1@gmail.com>
2022-12-09 10:42:06 -0500

    Fix dependencies

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,8 +7,8 @@ import org.chocosolver.parser.flatzinc.ChocoFZN;
 public class main {
 
     public static void main(String[] args) throws Exception {
-
-        ChocoFZN chocoFZN = new ChocoFZN();
-        chocoFZN.main(args);
+        System.out.println("rien a faire ici ");
+        //ChocoFZN chocoFZN = new ChocoFZN();
+        //chocoFZN.main(args);
     }
 }
