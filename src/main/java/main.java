62dbb7fb56f435e153744292c03c21fa7a21aa7a
Jules <juleslamy90@gmail.com>
2022-12-15 15:35:52 -0500

    Bench6

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,10 @@
 
 import data.Bench1;
 import data.Bench2;
+import data.Bench3;
+import data.Bench4;
+import data.Bench5;
+import data.Bench6;
 import data.Data;
 import models.Model4;
 import models.Model4TableauOnly;
@@ -10,7 +14,7 @@ public class main {
     public static void main(String[] args) throws Exception {
         //Data data = new Bench1();
         //Model4 model = new Model4();
-        Data data = new Bench2();
+        Data data = new Bench6();
         Model4TableauOnly model = new Model4TableauOnly();
         model.solve(data);
 
